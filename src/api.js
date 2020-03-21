9536a985261b84690d287d4a22c1d2b22262971c
Aaron.Berndt <Aaron.Berndt@target.com>
2020-03-21 09:08:11 -0500

    Made more dynamic

--- src/api.js
+++ src/api.js
@@ -1,26 +1,36 @@
 import RunMongoCommand from "./utilities/RunMongoCommand";
 
-export const GetAncestries = async ({ query: { name } }, res) => {
+export const GetAncestries = async ({ query }, res) => {
   try {
-    const query = name !== undefined ? { Name: name } : {};
-    const data = await RunMongoCommand("SHADOWS", query, "ancestries");
+    const data = await RunMongoCommand(query, "ancestries");
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json("Api Failed");
+  }
+};
+
+export const GetPaths = async ({ query }, res) => {
+  try {
+    const data = await RunMongoCommand(query, "paths");
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json(error);
   }
 };
 
-export const GetPaths = async ({ query: { type } }, res) => {
+export const GetSpells = async ({ query }, res) => {
   try {
-    return res.status(200).json(error);
+    const data = await RunMongoCommand(query, "spells");
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json(error);
   }
 };
 
-export const GetSpells = async ({ query: { tradition, level, type } }, res) => {
+export const GetCharacters = async ({ query }, res) => {
   try {
-    return res.status(200).json(error);
+    const data = await RunMongoCommand(query, "characters");
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json(error);
   }
