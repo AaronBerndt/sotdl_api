d5d380f4a30501700e43d2628122cf27ae9d56c1
Aaron.Berndt <Aaron.Berndt@target.com>
2020-06-16 07:06:23 -0500

    Removed logging

--- src/api.js
+++ src/api.js
@@ -41,7 +41,6 @@ export const InsertInto = async (
   res
 ) => {
   try {
-    console.log(collectionName, documentObject);
     const data = await RunMongoCommand(query, collectionName, "insert");
     return res.status(200).json(data);
   } catch (error) {
